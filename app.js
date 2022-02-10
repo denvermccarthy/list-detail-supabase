26d64a5c9e5772377b32f99a50e7be778795ea87
Denver McCarthy <dmichaelspdx@gmail.com>
2022-02-10 15:04:03 -0800

    started display food function and started testing appendFoodById

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
 import { 
+    getFoodByTier,
     getFoods,
 } from './fetch-utils.js';
 
 
-// getFoods();
+
+window.addEventListener('load', async() => {
+    const food = await getFoods();
+    console.log('hey', food);
+    // const cTier = await getFoodByTier('c');
+    // console.log('hy', cTier);
+});
+
+
+
+function displayFood(object) {
+    const a = document.createElement('a');
+    const div = document.createElement('div');
+    const h4 = document.createElement('h4');
+    const img = document.createElement('img');
+
+    h4.textContent = object.name;
+    img.src = `/assets/${object.name}`;
+    div.append(h4, img);
+    return a;
+}
+
+displayFood({
+    id: 1,
+    name: 'cereal',
+    tier: 'c',
+});
+
+function appendFoodById(html, object) {
+    const destination = document.getElementById(`${object.tier}-items`); 
+    // console.log(destination);    
+}
+
+appendFoodById(displayFood({ id: 1, name: 'cereal', tier: 'c', }), { id: 1, name: 'cereal', tier: 'c', });
+// function displayFoods(data) {
+
+// }
