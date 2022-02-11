9d1ffb91b8aa3b04c092c2505fb73d5198138b77
Denver McCarthy <dmichaelspdx@gmail.com>
2022-02-10 16:06:03 -0800

    displayed items in corresponding columns

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ import {
 
 
 
-window.addEventListener('load', async() => {
-    const food = await getFoods();
-    console.log('hey', food);
-    // const cTier = await getFoodByTier('c');
-    // console.log('hy', cTier);
-});
+
 
 
 
@@ -23,21 +18,46 @@ function displayFood(object) {
     h4.textContent = object.name;
     img.src = `/assets/${object.name}`;
     div.append(h4, img);
+    a.append(div);
     return a;
 }
 
-displayFood({
-    id: 1,
-    name: 'cereal',
-    tier: 'c',
-});
+// displayFood({
+//     id: 1,
+//     name: 'cereal',
+//     tier: 'c',
+// });
 
-function appendFoodById(html, object) {
-    const destination = document.getElementById(`${object.tier}-items`); 
-    // console.log(destination);    
-}
+ 
 
-appendFoodById(displayFood({ id: 1, name: 'cereal', tier: 'c', }), { id: 1, name: 'cereal', tier: 'c', });
+// appendFoodById(displayFood({ id: 1, name: 'cereal', tier: 'c', }), { id: 1, name: 'cereal', tier: 'b', });
 // function displayFoods(data) {
 
+// }
+
+window.addEventListener('load', async() => {
+    const food = await getFoods();
+    console.log('hey', food);
+    
+
+    for (let item of food) {
+        console.log('items', item);
+        
+        let destination = document.getElementById(`${item.tier}-items`);
+        console.log('destination', destination);
+
+        const html = displayFood(item);
+        console.log('html', html);
+
+        destination.append(html);
+    }
+    // const cTier = await getFoodByTier('c');
+    // console.log('hy', cTier);
+});
+
+// function appendFoodById(html, object) {
+//     const destination = await document.getElementById(`${object.tier}-items`); 
+//     console.log('destination', destination);
+//     await destination.append(html);
+//     return destination;
 // }
